8e10011eb2d548f0309fedcf3d7aac4770e7dcdc
YeJun, Jung <yejun614@naver.com>
2021-07-03 13:40:37 +0000

    Fix some bugs

--- problems/baekjoon/p1931.cpp
+++ problems/baekjoon/p1931.cpp
@@ -4,23 +4,23 @@
  */
 
 #include <cstdio>
-#include <map>
+#include <set>
 using namespace std;
 
 int main() {
 	int N;
 	scanf("%d", &N);
 
-	map<int, int> meets;
+	multiset<pair<int, int> > meets;
 	int start, end;
 
 	for (int i=0; i<N; i++) {
 		scanf("%d %d", &start, &end);
-		meets.insert(pair<int, int>(end, start));
+		meets.insert(make_pair(end, start));
 	}
 
 	int answer=0, current=0;
-	map<int, int>::iterator it=meets.begin();
+	multiset<pair<int, int> >::iterator it=meets.begin();
 
 	while (it != meets.end()) {
 		for (it=meets.begin(); it!=meets.end(); it++) {

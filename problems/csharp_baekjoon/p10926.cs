40aee8e518d89a8b641366d68ed41eeaccff882d
YeJun, Jung <yejunjung98@gmail.com>
2022-04-13 01:43:09 +0900

    Add 10926 solution

---
+++ problems/csharp_baekjoon/p10926.cs
@@ -0,0 +1,19 @@
+/*
+ * (220413) ??!
+ * https://www.acmicpc.net/problem/10926
+ */
+
+using System;
+
+namespace Baekjoon
+{
+  class Program
+  {
+    static void Main()
+    {
+      string str = Console.ReadLine();
+
+      Console.WriteLine($"{str}??!");
+    }
+  }
+}

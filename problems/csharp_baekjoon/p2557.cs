58aad373644622f8110152d1ebbfc7bc45b7f3cd
YeJun, Jung <yejunjung98@gmail.com>
2022-04-13 00:53:51 +0900

    Start C# algorithm study

---
+++ problems/csharp_baekjoon/p2557.cs
@@ -0,0 +1,18 @@
+/*
+ * (220413) Hello World
+ * https://www.acmicpc.net/problem/2557
+ *
+ * C# 으로 알고리즘 공부하기!
+ */
+
+using System;
+
+namespace Baekjoon
+{
+  class Program
+  {
+    static void Main() {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}

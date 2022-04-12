c8c8bd85eb2a29def4e45b410f89ff9bc50e40f2
YeJun, Jung <yejunjung98@gmail.com>
2022-04-13 01:04:54 +0900

    Add 10171 solution

---
+++ problems/csharp_baekjoon/p10171.cs
@@ -0,0 +1,23 @@
+/*
+ * (220413) 고양이
+ * https://www.acmicpc.net/problem/10171
+ *
+ * [풀이]
+ * 일부 특수문자는 escape sequence가 필요하다.
+ */
+
+using System;
+
+namespace Baekjoon
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("\\    /\\");
+      Console.WriteLine(" )  ( \')");
+      Console.WriteLine("(  /  )");
+      Console.WriteLine(" \\(__)|");
+    }
+  }
+}

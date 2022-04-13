fb844116ed5fead9867b7c14543fe9c0197d43c5
YeJun, Jung <yejunjung98@gmail.com>
2022-04-14 01:21:24 +0900

    Add 11021 solution

---
+++ problems/csharp_baekjoon/p11021.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Baekjoon
+{
+  class Program
+  {
+    static void Main()
+    {
+      int T = int.Parse(Console.ReadLine());
+
+      for (int i = 1; i <= T; i ++) {
+        int [] nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        Console.WriteLine("Case #{0}: {1}", i, nums[0] + nums[1]);
+      }
+    }
+  }
+}

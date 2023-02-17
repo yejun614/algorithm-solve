f8562f96d8805f11e40d215306383d4ed03f2883
YeJun, Jung <yejun614@naver.com>
2023-02-17 23:27:51 +0900

    Add baekjoon p25083

---
+++ problems/baekjoon/p25083.cpp
@@ -0,0 +1,18 @@
+/*
+ * (230217) 새싹
+ * https://www.acmicpc.net/problem/25083
+ */
+
+#include <cstdio>
+
+int main() {
+  printf("         ,r'\"7\n");
+  printf("r`-_   ,'  ,/\n");
+  printf(" \\. \". L_r'\n");
+  printf("   `~\\/\n");
+  printf("      |\n");
+  printf("      |\n");
+
+  return 0;
+}
+
